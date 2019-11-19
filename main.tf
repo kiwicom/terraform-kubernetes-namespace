7202d2bbc31532c0b8bb7eaa1d4fafa1c024b864
Dražen Perić <drazen.peric@kiwi.com>
2019-11-19 13:20:47 +0100

    Add orphan role (#4)
    * Change token policy
    
    * Add no_parent=true
    
    * add no_parent = true to role

--- main.tf
+++ main.tf
@@ -70,9 +70,17 @@ path "secret/gcp-project/${var.project_id}/ns-${var.name}-*" {
 EOT
 }
 
+resource "vault_token_auth_backend_role" "project_namespace_role" {
+  count            = (var.project_id == "" || var.vault_path == "") ? 0 : 1
+  role_name        = "tf-gcp-projects-${var.project_id}-${var.name}-read"
+  allowed_policies = [vault_policy.project_namespace_policy[0].name]
+  orphan           = true
+}
+
 resource "vault_token" "project_namespace_token" {
   count             = (var.project_id == "" || var.vault_path == "") ? 0 : 1
   display_name      = "tf-gcp-projects-${var.project_id}-${var.name}-read"
+  role_name         = vault_token_auth_backend_role.project_namespace_role[0].role_name
   policies          = [vault_policy.project_namespace_policy[0].name]
   no_default_policy = true
   renewable         = true
